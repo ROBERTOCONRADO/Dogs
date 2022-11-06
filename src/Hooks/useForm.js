63c016a00b479923852bdba5312601ce2e55e27c
Roberto Conrado <robertoconradorc@gmail.com>
2022-11-06 16:48:49 -0300

    background, display-grid

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha com um email válido.',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha precisa ter no mínimo 8 caracteres, contendo caracter maiúsculo, minúsculo e número.',
+  },
 };
 
 const useForm = (type) => {
