521905718b90671f1e92d04f41629c99201609ba
Roberto Conrado <robertoconradorc@gmail.com>
2022-11-06 18:14:16 -0300

    perfil/usuario

---
+++ src/Components/User/UserHeaderNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const UserHeaderNav = () => {
+  return (
+    <nav>
+      <NavLink>Feed</NavLink>
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
