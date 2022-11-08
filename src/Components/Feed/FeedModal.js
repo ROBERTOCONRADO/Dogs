c962aa5eafccad4ae5516de920687d3d219a9da0
Roberto Conrado <robertoconradorc@gmail.com>
2022-11-08 12:58:35 -0300

    modal-fotos/svg/(feed)

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTO_GET } from '../../api';
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+  return (
+    <div className={styles.modal}>
+      <img src={photo.src} alt="" />
+    </div>
+  );
 };
 
 export default FeedModal;
--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,24 +6,30 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
-      console.log(json);
+      const total = 6;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
+
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
