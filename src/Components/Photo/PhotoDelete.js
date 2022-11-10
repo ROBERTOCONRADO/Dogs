784f76b52ac9d9e8cabbb99f7ce7b52336268e1f
Roberto Conrado <robertoconradorc@gmail.com>
2022-11-10 15:24:37 -0300

    Loading-excited

--- src/Components/Photo/PhotoDelete.js
+++ src/Components/Photo/PhotoDelete.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../Hooks/useFetch';
 
 const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm('Deseja mesmo deletar?');
+    if (confirm === true) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+
   return (
-    <div>
-      <button className={styles.delete}>Deletar</button>
-    </div>
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}></button>
+      )}
+    </>
   );
 };
 
